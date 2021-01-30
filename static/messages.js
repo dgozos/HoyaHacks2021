cba0fdf64826c43cda1339dad5364267d3bf96e6
Kyle <kyleedwardgjava@gmail.com>
2021-01-29 21:51:42 -0800

    Added more JS files

--- static/messages.js
+++ static/messages.js
@@ -1,12 +1,29 @@
-function sendMessageToUser(username , note){
+var database = firebase.database();
+function sendMessageToUser(){
+  username = 'kylejava';
+  note = 'this is a test';
   //var username = document.getElementById('userName').innerHTML;
   //var note = document.getElementById('note').innerHTML;
   var postListRef = firebase.database().ref('users/' + username +'/newMessages');
   var newPostRef = postListRef.push();
   newPostRef.set({
-      entry:note,
+      newNote:note,
   });
   console.log("test worked");
 }
 
-sendMessageToUser('kylejava' , 'This is a test');
+function moveNewMessageToOld(){
+  var message = document.getElementById('message').innerHTML;
+  var postListRef = firebase.database().ref('users/' + username +'/oldMessages');
+  var newPostRef = postListRef.push();
+  newPostRef.set({
+      entry:message,
+  });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+  } else {
+    sendMessageToUser();
+  }
+});
