b56938fbae761a6e315dc5a26f973ac46ad61539
Kyle <kyleedwardgjava@gmail.com>
2021-01-29 18:14:20 -0800

    added app.yaml

---
+++ static/SignIn.js
@@ -0,0 +1,17 @@
+
+function SignIn(){
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user) => {
+
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      document.getElementById('error').innerHTML = errorMessage;
+    });
+}
